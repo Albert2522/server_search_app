e107442ea9f745f205b74addba4a3161e2e845fc
Albert2522 <5043186@mail.com>
2017-04-05 23:13:20 -0700

    added to heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const craigs = require('node-craigslist');
+
+client = new craigs.Client({
+    city : 'sfbay'
+  });
+
+app.get('/', (request, response) => {
+  client.list().then(listings => response.send(listings));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
