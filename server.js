24d3942b1c7dfbbc825ba19868effd99973c6d31
Albert2522 <5043186@mail.com>
2017-04-06 10:08:50 -0700

    add functionality

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 const craigs = require('node-craigslist');
 
-client = new craigs.Client({
-    city : 'sfbay'
-  });
 
 app.get('/craigslist', (request, response) => {
-  client.list().then(listings => response.send(listings));
+  let query = request.query;
+  let user_city = query.city ? query.city : 'sfbay'
+  client = new craigs.Client({
+    city : user_city
+  });
+  console.log(user_city);
+  let options = {maxAsk: query.maxAsk, minAsk: query.minAsk};
+  if (query.search) {
+    client.search(options, query.search).then(listings => response.send(listings));
+  } else {
+    client.list().then(listings => response.send(listings));
+  }
 });
 
 app.listen(port, (err) => {
