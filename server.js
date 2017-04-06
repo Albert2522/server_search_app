c9eba7ba18dae2b221ca5508648317095e93b729
Albert2522 <5043186@mail.com>
2017-04-06 10:57:57 -0700

    add image scrapper

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const craigs = require('node-craigslist');
-
+const imgrab = require('imagegrab');
+const sendResponseCraigs = (response, listings) => {
+  let final_response = [ ];
+  listings.map(listing => {
+    let tmp = listing;
+    imgrab(listing.url, url => {
+      if (listing.hasPic) {
+        tmp.image_url = url[0];
+      } else {
+        tmp.image_url = '';
+      }
+      final_response.push(tmp);
+      if (final_response.length == listings.length) {
+        response.send(final_response);
+      }
+    });
+  });
+};
 
 app.get('/craigslist', (request, response) => {
   let query = request.query;
@@ -10,12 +27,11 @@ app.get('/craigslist', (request, response) => {
   client = new craigs.Client({
     city : user_city
   });
-  console.log(user_city);
   let options = {maxAsk: query.maxAsk, minAsk: query.minAsk};
   if (query.search) {
-    client.search(options, query.search).then(listings => response.send(listings));
+    client.search(options, query.search).then(listings => sendResponseCraigs(response, listings));
   } else {
-    client.list().then(listings => response.send(listings));
+    client.list().then(listings => sendResponseCraigs(response, listings));
   }
 });
 
