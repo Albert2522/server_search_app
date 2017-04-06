45e130ee0f01fbd5187cefd7e7d6e50a4aaff294
Albert2522 <5043186@mail.com>
2017-04-06 12:29:31 -0700

    add ebay service

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 const craigs = require('node-craigslist');
 const imgrab = require('imagegrab');
+const ebay = require('ebay-api');
+
+
 const sendResponseCraigs = (response, listings) => {
   let final_response = [ ];
   listings.map(listing => {
@@ -21,6 +24,15 @@ const sendResponseCraigs = (response, listings) => {
   });
 };
 
+const ebayImageUrlPars = (array_url) => {
+  array_url.forEach( url => {
+    if (url.includes('s-l300.jpg')) {
+      return url;
+    }
+  })
+  return "";
+};
+
 app.get('/craigslist', (request, response) => {
   let query = request.query;
   let user_city = query.city ? query.city : 'sfbay'
@@ -33,6 +45,51 @@ app.get('/craigslist', (request, response) => {
   } else {
     client.list().then(listings => sendResponseCraigs(response, listings));
   }
+});
+
+app.get('/ebay', (request, response) => {
+  let keys  = request.query.search? request.query.search.split(" ") : "car";
+  var params = {
+    keywords: keys,
+
+    // add additional fields
+    outputSelector: ['AspectHistogram'],
+
+    paginationInput: {
+      entriesPerPage: 120
+    }
+  };
+
+  ebay.xmlRequest({
+      serviceName: 'Finding',
+      opType: 'findItemsByKeywords',
+      appId: 'AlbertAb-simplese-PRD-e090738eb-8c1e10ef',      // FILL IN YOUR OWN APP KEY, GET ONE HERE: https://publisher.ebaypartnernetwork.com/PublisherToolsAPI
+      params: params,
+      parser: ebay.parseResponseJson    // (default)
+    },
+    // gets all the items together in a merged array
+    function itemsCallback(error, itemsResponse) {
+      if (error) throw error;
+
+      console.log(itemsResponse);
+      var items = itemsResponse.searchResult.item;
+      let final_response = [ ];
+      items.map( item => {
+        let tmp = {};
+        tmp.category = item.primaryCategory.categoryName;
+        tmp.date = item.listingInfo.endTime;
+        tmp.hasPick = item.galleryURL ? true : false;
+        tmp.pid = item.itemId;
+        tmp.location = item.location;
+        tmp.price = item.sellingStatus.currentPrice.amount;
+        tmp.title = item.title;
+        tmp.url = item.viewItemURL;
+        tmp.image_url = item.galleryURL;
+        final_response.push(tmp);
+      });
+      response.send(final_response);
+    }
+  );
 });
 
 app.listen(port, (err) => {
