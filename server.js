dd9e5fe07d4c48f2cbb0b5b184b6f9b58f1630be
Albert2522 <5043186@mail.com>
2017-04-23 21:16:09 -0700

    fix ebay http https

--- server.js
+++ server.js
@@ -95,8 +95,8 @@ app.get('/ebay', (request, response) => {
               tmp.price = tmp.price.slice(0, -3);
             }
             tmp.title = item.title;
-            tmp.url = item.viewItemURL.substring(0, 4) + 's' + item.viewItemURL(4);
-            tmp.image_url = item.galleryURL;
+            tmp.url = item.viewItemURL.substring(0, 4) + 's' + item.viewItemURL.substring(4);
+            tmp.image_url = item.galleryURL.substring(0, 4) + 's' + item.galleryURL.substring(4);
             final_response.push(tmp);
           });
           response.send(final_response);
